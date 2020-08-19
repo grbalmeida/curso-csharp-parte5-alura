edec073e02a336aed051f4982ba0cb5b515ea49a
gilvan <g.r.almeida@live.com>
2020-08-19 10:29:46 -0300

    Referências de projetos

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace ByteBank.SistemaAgencia
+﻿using ByteBank.Modelos;
+using System;
+
+namespace ByteBank.SistemaAgencia
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
         }
     }
 }
