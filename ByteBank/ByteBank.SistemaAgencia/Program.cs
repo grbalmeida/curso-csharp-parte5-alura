b5d86079c314582431e0279f329bc4c167a96be8
gilvan <g.r.almeida@live.com>
2020-08-19 21:56:36 -0300

    Lidando com datas

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -7,11 +7,37 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 489754);
+            DateTime dataFimPagamento = new DateTime(2020, 9, 20);
+            DateTime dataCorrente = DateTime.Now;
 
-            Console.WriteLine(conta.Numero);
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + GetIntervaloDeTempoLegivel(diferenca);
+
+            Console.WriteLine(mensagem);
 
             Console.ReadLine();
+        }
+
+        static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        {
+            if (timeSpan.Days > 30)
+            {
+                int quantidadeMeses = timeSpan.Days / 30;
+                
+                if (quantidadeMeses == 1)
+                {
+                    return "1 mês";
+                }
+                
+                return quantidadeMeses + " meses";
+            }
+            else if (timeSpan.Days > 7)
+            {
+                int quantidadeSemanas = timeSpan.Days / 7;
+            }
+
+            return timeSpan.Days + " dias";
         }
     }
 }
